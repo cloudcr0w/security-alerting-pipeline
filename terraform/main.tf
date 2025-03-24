d3f6aa2d19e5fc180431bacecc1f54fa743a44af
cloudcr0w <adamwronowy@gmail.com>
2025-03-24 14:28:19 +0800

    edited main, added region variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-#Eventbridge rule to create user
-
-resource "aws_cloudwatch_event_rule" "iam_create_user" {
-  name        = "iam-create-user-rule"
-  description = "Trigger on IAM CreateUser events"
-  event_pattern = jsonencode({
-    source      = ["aws.iam"],
-    detail-type = ["AWS API Call via CloudTrail"],
-    detail = {
-      eventName = ["CreateUser"]
-    }
-  })
-}
-resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.iam_create_user.name
-  target_id = "LambdaFunction"
-  arn       = aws_lambda_function.alert_function.arn
-}
-
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.alert_function.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.iam_create_user.arn
+  region = var.region
 }
