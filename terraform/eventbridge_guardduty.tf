a9f753b9dd1c1c9586a31d158af0e9c99e38b28c
cloudcr0w <adamwronowy@gmail.com>
2025-03-26 14:46:26 +0800

    added severity (for practice) >=5 filter to GuardDuty EventBridge rule

--- terraform/eventbridge_guardduty.tf
+++ terraform/eventbridge_guardduty.tf
@@ -1,8 +1,15 @@
+# Eventbridge rule to get an event from Guardduty
+
 resource "aws_cloudwatch_event_rule" "guardduty_finding" {
   name        = "guardduty_finding"
   description = "Catches all GuardDuty findings for further alerting"
   event_pattern = jsonencode({
     source      = ["aws.guardduty"],
-    detail-type = ["GuardDuty Finding"]
+    detail-type = ["GuardDuty Finding"],
+    detail = {
+      severity = [{
+        numeric = [">=", 5]
+      }]
+    }
   })
 }
