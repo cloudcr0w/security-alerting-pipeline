f700c0691143631340c3a658a81e90ac8d03be7b
cloudcr0w <adamwronowy@gmail.com>
2025-03-26 15:45:25 +0800

    add separate lambda function for guardduty alerts

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,3 +14,18 @@ resource "aws_lambda_function" "alert_function" {
     }
   }
 }
+
+resource "aws_lambda_function" "guardduty_alert_function" {
+  filename         = "../lambda/guardduty_alert_function.zip"
+  function_name    = "guardduty_alert_function"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "guardduty_alert_function.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = filebase64sha256("../lambda/guardduty_alert_function.zip")
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.security_alerts.arn
+    }
+  }
+}
