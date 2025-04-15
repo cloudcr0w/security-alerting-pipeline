dd27a83d85178d27ace61328fff7a8f479cf7ce5
Adam Wrona <adamwronowy@gmail.com>
2025-04-15 12:45:27 +0800

    added config.tf, AWS Config with S3 bucket and recorder

---
+++ terraform/config.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "config_logs" {
+  bucket = "aws-config-logs-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+
+  tags = {
+    Name        = "AWS Config Logs"
+    Environment = "security-pipeline"
+  }
+}
+
+resource "aws_config_configuration_recorder" "main" {
+  name     = "default"
+  role_arn = aws_iam_role.config.arn
+
+  recording_group {
+    all_supported = true
+    include_global_resource_types = true
+  }
+}
+
+resource "aws_config_delivery_channel" "main" {
+  name           = "default"
+  s3_bucket_name = aws_s3_bucket.config_logs.bucket
+  depends_on     = [aws_config_configuration_recorder.main]
+}
+
+resource "aws_config_configuration_recorder_status" "main" {
+  name       = aws_config_configuration_recorder.main.name
+  is_enabled = true
+}
+
+resource "aws_iam_role" "config" {
+  name = "aws_config_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "config.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "config_policy" {
+  role       = aws_iam_role.config.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
