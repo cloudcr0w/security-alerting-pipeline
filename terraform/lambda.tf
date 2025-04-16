4cf9cb15ba9f7db0999c39451ac515d019aca9bd
Adam Wrona <adamwronowy@gmail.com>
2025-04-16 17:43:11 +0800

    add basic handler for AWS Config alerts

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -47,3 +47,28 @@ resource "aws_lambda_function" "guardduty_alert_function" {
   }
 
 }
+resource "aws_lambda_function" "aws_config_handler" {
+  filename         = "lambda/aws_config_handler.zip"
+  function_name    = "aws_config_handler"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "aws_config_handler.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = filebase64sha256("lambda/aws_config_handler.zip")
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
