ca39a2e0b18b0a0b6f2238bcb6b5d65946287bff
Adam Wrona <adamwronowy@gmail.com>
2025-04-16 18:23:50 +0800

    prepare ZIP deployment and adjust Lambda definition

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -48,12 +48,12 @@ resource "aws_lambda_function" "guardduty_alert_function" {
 
 }
 resource "aws_lambda_function" "aws_config_handler" {
-  filename         = "lambda/aws_config_handler.zip"
+  filename         = "../lambda/aws_config_handler.zip"
   function_name    = "aws_config_handler"
   role             = aws_iam_role.lambda_exec.arn
   handler          = "aws_config_handler.lambda_handler"
   runtime          = "python3.12"
-  source_code_hash = filebase64sha256("lambda/aws_config_handler.zip")
+  source_code_hash = filebase64sha256("../lambda/aws_config_handler.zip")
 }
 
 resource "aws_iam_role" "lambda_exec" {
@@ -61,9 +61,9 @@ resource "aws_iam_role" "lambda_exec" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect    = "Allow",
+      Effect = "Allow",
       Principal = { Service = "lambda.amazonaws.com" },
-      Action    = "sts:AssumeRole"
+      Action = "sts:AssumeRole"
     }]
   })
 }
