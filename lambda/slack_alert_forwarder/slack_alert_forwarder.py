c7e98f6698a22a709c55960f01e72ec43731c3b8
Adam Wrona <adamwronowy@gmail.com>
2025-06-27 12:57:28 +0800

    Add Slack alert forwarder Lambda function

---
+++ lambda/slack_alert_forwarder/slack_alert_forwarder.py
@@ -0,0 +1,23 @@
+import json
+import os
+import urllib3
+
+http = urllib3.PoolManager()
+SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
+
+def lambda_handler(event, context):
+    for record in event["Records"]:
+        msg = record["Sns"]["Message"]
+        payload = {
+            "text": f"🚨 *SNS Alert received!*\n```{msg}```"
+        }
+        encoded_msg = json.dumps(payload).encode("utf-8")
+        resp = http.request(
+            "POST",
+            SLACK_WEBHOOK_URL,
+            body=encoded_msg,
+            headers={"Content-Type": "application/json"},
+            timeout=10.0 
+)
+
+    print(f"Slack response: {resp.status}")
