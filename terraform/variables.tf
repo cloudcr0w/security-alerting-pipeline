5cc4b8566234a8c17f65637b5f230e569d43f224
Adam Wrona <adamwronowy@gmail.com>
2025-09-29 18:27:55 +0800

    feat(terraform): add validation for lambda_timeout variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,37 +1,41 @@
-+ # General settings
- variable "region" {
-   description = "AWS region to deploy resources"
-   type        = string
-   default     = "us-east-1"
- }
+# General settings
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
- variable "alert_email" {
-   description = "Email address to receive security alerts"
-   type        = string
- }
+variable "alert_email" {
+  description = "Email address to receive security alerts"
+  type        = string
+}
 
-+ # Lambda configuration
- variable "lambda_timeout" {
-   description = "Maximum execution time for Lambda in seconds"
-   type        = number
-   default     = 10
- }
+# Lambda configuration
+variable "lambda_timeout" {
+  description = "Maximum execution time for Lambda in seconds"
+  type        = number
+  default     = 10
+  validation {
+    condition     = var.lambda_timeout <= 900
+    error_message = "Lambda timeout cannot exceed 900 seconds."
+  }
+}
 
- variable "lambda_memory_size" {
-   description = "Amount of memory in MB for Lambda"
-   type        = number
-   default     = 256
- }
+variable "lambda_memory_size" {
+  description = "Amount of memory in MB for Lambda"
+  type        = number
+  default     = 256
+}
 
-+ # Integrations
- variable "slack_webhook_url" {
-   description = "Slack incoming webhook URL"
-   sensitive   = true
- }
+# Integrations
+variable "slack_webhook_url" {
+  description = "Slack incoming webhook URL"
+  sensitive   = true
+}
 
-+ # Logging
- variable "cloudtrail_log_group_name" {
-   description = "Name of the CloudTrail log group"
-   type        = string
-   default     = "/aws/cloudtrail/my-cloudtrail-log-group"
- }
+# Logging
+variable "cloudtrail_log_group_name" {
+  description = "Name of the CloudTrail log group"
+  type        = string
+  default     = "/aws/cloudtrail/my-cloudtrail-log-group"
+}
