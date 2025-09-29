6c54806bf989da65aaffb3873415e42815ec9f18
Adam Wrona <adamwronowy@gmail.com>
2025-09-29 09:41:22 +0800

    chore(terraform): group variables by section for better readability

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,37 @@
-variable "region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-}
++ # General settings
+ variable "region" {
+   description = "AWS region to deploy resources"
+   type        = string
+   default     = "us-east-1"
+ }
 
-variable "alert_email" {
-  description = "Email address to receive security alerts"
-  type        = string
-}
-variable "lambda_timeout" {
-  description = "Maximum execution time for Lambda in seconds"
-  type        = number
-  default     = 10
-}
+ variable "alert_email" {
+   description = "Email address to receive security alerts"
+   type        = string
+ }
 
-variable "lambda_memory_size" {
-  description = "Amount of memory in MB for Lambda"
-  type        = number
-  default     = 256
-}
-variable "slack_webhook_url" {
-  description = "Slack incoming webhook URL"
-  sensitive   = true
-}
++ # Lambda configuration
+ variable "lambda_timeout" {
+   description = "Maximum execution time for Lambda in seconds"
+   type        = number
+   default     = 10
+ }
 
-variable "cloudtrail_log_group_name" {
-  description = "Name of the CloudTrail log group"
-  type        = string
-  default     = "/aws/cloudtrail/my-cloudtrail-log-group"
-}
+ variable "lambda_memory_size" {
+   description = "Amount of memory in MB for Lambda"
+   type        = number
+   default     = 256
+ }
+
++ # Integrations
+ variable "slack_webhook_url" {
+   description = "Slack incoming webhook URL"
+   sensitive   = true
+ }
+
++ # Logging
+ variable "cloudtrail_log_group_name" {
+   description = "Name of the CloudTrail log group"
+   type        = string
+   default     = "/aws/cloudtrail/my-cloudtrail-log-group"
+ }
