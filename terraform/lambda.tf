204bc914229ec54091efdd8459d6c2fc2108a068
Adam Wrona <adamwronowy@gmail.com>
2026-03-02 13:08:23 +0100

    Refactor Lambda packaging to use archive_file (CI-friendly)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,38 +1,70 @@
-#Lambda function
+############################################
+# Lambda packaging 
+############################################
+
+data "archive_file" "alert_function_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda_src/alert_function"
+  output_path = "${path.module}/.artifacts/alert_function.zip"
+}
+
+data "archive_file" "guardduty_alert_function_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda_src/guardduty_alert_function"
+  output_path = "${path.module}/.artifacts/guardduty_alert_function.zip"
+}
+
+data "archive_file" "aws_config_handler_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda_src/aws_config_handler"
+  output_path = "${path.module}/.artifacts/aws_config_handler.zip"
+}
+
+data "archive_file" "slack_alert_forwarder_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda_src/slack_alert_forwarder"
+  output_path = "${path.module}/.artifacts/slack_alert_forwarder.zip"
+}
+
+############################################
+# Lambda: Security alert function -> SNS
+############################################
 
 resource "aws_lambda_function" "alert_function" {
-  filename         = "../lambda_src/alert_function/alert_function.zip"
-  function_name    = "security-alert-function"
-  role             = aws_iam_role.lambda_exec_role_v2.arn
-  handler          = "alert_function.lambda_handler"
-  runtime          = "python3.12"
-  source_code_hash = filebase64sha256("../lambda_src/alert_function/alert_function.zip")
-  memory_size      = var.lambda_memory_size
-  timeout          = var.lambda_timeout
-  tags             = local.common_tags
+  function_name = "security-alert-function"
+  role          = aws_iam_role.lambda_exec_role_v2.arn
+  handler       = "alert_function.lambda_handler"
+  runtime       = "python3.12"
 
+  filename         = data.archive_file.alert_function_zip.output_path
+  source_code_hash = data.archive_file.alert_function_zip.output_base64sha256
 
-
+  memory_size = var.lambda_memory_size
+  timeout     = var.lambda_timeout
+  tags        = local.common_tags
 
   environment {
     variables = {
       SNS_TOPIC_ARN = aws_sns_topic.security_alerts.arn
     }
   }
-
-
 }
 
-resource "aws_lambda_function" "guardduty_alert_function" {
-  filename         = "../lambda_src/guardduty_alert_function/guardduty_alert_function.zip"
-  function_name    = "guardduty_alert_function"
-  role             = aws_iam_role.lambda_exec_role_v2.arn
-  handler          = "guardduty_alert_function.lambda_handler"
-  runtime          = "python3.12"
-  source_code_hash = filebase64sha256("../lambda_src/guardduty_alert_function/guardduty_alert_function.zip")
-  memory_size      = var.lambda_memory_size
-  timeout          = var.lambda_timeout
+############################################
+# Lambda: GuardDuty handler -> SNS (+ optional env)
+############################################
 
+resource "aws_lambda_function" "guardduty_alert_function" {
+  function_name = "guardduty_alert_function"
+  role          = aws_iam_role.lambda_exec_role_v2.arn
+  handler       = "guardduty_alert_function.lambda_handler"
+  runtime       = "python3.12"
+
+  filename         = data.archive_file.guardduty_alert_function_zip.output_path
+  source_code_hash = data.archive_file.guardduty_alert_function_zip.output_base64sha256
+
+  memory_size = var.lambda_memory_size
+  timeout     = var.lambda_timeout
 
 
   environment {
@@ -42,23 +74,20 @@ resource "aws_lambda_function" "guardduty_alert_function" {
     }
   }
 
+  # Keeping your explicit tags as in the original file.
   tags = {
     Project     = "SecurityAlertingPipeline"
     Environment = "dev"
   }
+}
 
-}
-resource "aws_lambda_function" "aws_config_handler" {
-  filename         = "../lambda_src/aws_config_handler/aws_config_handler.zip"
-  function_name    = "aws_config_handler"
-  role             = aws_iam_role.lambda_exec.arn
-  handler          = "aws_config_handler.lambda_handler"
-  runtime          = "python3.12"
-  source_code_hash = filebase64sha256("../lambda_src/aws_config_handler/aws_config_handler.zip")
-}
+############################################
+# Separate execution role (as in your original file)
+############################################
 
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role_final"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -68,28 +97,45 @@ resource "aws_iam_role" "lambda_exec" {
     }]
   })
 }
-# Get AWS Account ID for Secrets Manager ARN
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "slack_alert_forwarder" {
-  filename         = "../lambda_src/slack_alert_forwarder/slack_alert_forwarder.zip"
-  function_name    = "slack_alert_forwarder"
-  role             = aws_iam_role.lambda_exec.arn
-  handler          = "slack_alert_forwarder.lambda_handler"
-  runtime          = "python3.12"
-  source_code_hash = filebase64sha256("../lambda_src/slack_alert_forwarder/slack_alert_forwarder.zip")
-  memory_size      = var.lambda_memory_size
-  timeout          = var.lambda_timeout
-  tags             = local.common_tags
+############################################
+# Lambda: AWS Config handler
+############################################
 
-  environment {
-    variables = {
-      # no env
-    }
-  }
+resource "aws_lambda_function" "aws_config_handler" {
+  function_name = "aws_config_handler"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "aws_config_handler.lambda_handler"
+  runtime       = "python3.12"
+
+  filename         = data.archive_file.aws_config_handler_zip.output_path
+  source_code_hash = data.archive_file.aws_config_handler_zip.output_base64sha256
 }
 
+############################################
+# Lambda: Slack alert forwarder
+############################################
+
+resource "aws_lambda_function" "slack_alert_forwarder" {
+  function_name = "slack_alert_forwarder"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "slack_alert_forwarder.lambda_handler"
+  runtime       = "python3.12"
+
+  filename         = data.archive_file.slack_alert_forwarder_zip.output_path
+  source_code_hash = data.archive_file.slack_alert_forwarder_zip.output_base64sha256
+
+  memory_size = var.lambda_memory_size
+  timeout     = var.lambda_timeout
+  tags        = local.common_tags
+
+ 
+  environment {
+    variables = {}
+  }
+}
