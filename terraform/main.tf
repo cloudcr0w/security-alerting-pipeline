c0a102c6f8f56853831be8227aac493fc8d4acdf
Adam Wrona <adamwronowy@gmail.com>
2026-03-02 13:08:05 +0100

    Add archive provider for Lambda packaging

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.4.0"
+    }
+  }
+}
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
