b3b144268c469fa0d244dc4a9aa05f0aa3424e91
Adam Wrona <adamwronowy@gmail.com>
2026-03-10 10:04:35 +0100

    Format Python codebase using Black

--- alert-receiver/main.py
+++ alert-receiver/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/alert", methods=["POST"])
 def receive_alert():
     data = request.get_json()
@@ -9,8 +10,10 @@ def receive_alert():
     print(data)
     return {"message": "Alert received"}, 200
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 @app.route("/version", methods=["GET"])
 def version():
--- lambda_src/alert_function/alert_function.py
+++ lambda_src/alert_function/alert_function.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
     event_name = event["detail"].get("eventName", "unknown-event")
 
     message = f"""
-🚨 [SECURITY ALERT]
+ [SECURITY ALERT]
 Event: {event_name}
 User: {user}
 Time: {time}
